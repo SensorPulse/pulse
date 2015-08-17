ab0ef580b01c132b2aa1d9947d6f7ec408a149b2
fangmobile <jian_fang@intuit.com>
2015-08-17 02:48:24 -0700

    use html template. add stuff in css

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,7 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import play.twirl.api.Html;
 import views.html.*;
 
 public class Application extends Controller {
@@ -12,7 +13,8 @@ public class Application extends Controller {
     }
 
     public Result testPage() {
-        return ok("<h1>Pulse Sensor</h1>").as("text/html");
+        //return ok("<h1>Pulse Sensor</h1>").as("text/html");
+        return ok(views.html.main.render("pulse sensor", new Html("<h1>coen 315 project </h1>")));
     }
 
 }
