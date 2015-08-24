256f9acec139907c54c35fc708ff1524f7505136
fangmobile <jfang1@MTVL130e6095f.local>
2015-08-23 23:02:18 -0700

    add data to data.txt file using append. So to clean the data clean the content in data.txt, do not remove file

--- app/controllers/Data.java
+++ app/controllers/Data.java
@@ -1,13 +1,26 @@
 package controllers;
 
 
+import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class Data extends Controller {
     public Result save(int data){
-        System.out.println("received data: " + data);
-
+        Logger.info("received data: " + data);
+        Path path = Paths.get("./data.txt");
+        try {
+            Files.write(path, (String.valueOf(data) + "\n").getBytes() , StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            Logger.error("file io error when writing data to file");
+            e.printStackTrace();
+        }
         return ok();
     }
 }
