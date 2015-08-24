21dc8a23131a902c88352b7fc46e62deb6352e4e
fangmobile <fangjian@gmail.com>
2015-08-23 23:16:59 -0700

    read data if /home is accessed

--- app/controllers/Home.java
+++ app/controllers/Home.java
@@ -5,10 +5,32 @@ import play.mvc.*;
 
 import views.html.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Home extends Controller {
 
     public Result index() {
+        readData();
         return ok(home.render());
+    }
+
+    private void readData(){
+        Logger.info("retrieving data from file");
+        Path path = Paths.get("./data.txt");
+
+        try {
+            List<String> dataList = Files.readAllLines(path);
+            for (String dataString : dataList){
+                Logger.info(dataString);
+            }
+        }catch (IOException e) {
+            Logger.error("file io error when reading data from file");
+            e.printStackTrace();
+        }
     }
 
 }
