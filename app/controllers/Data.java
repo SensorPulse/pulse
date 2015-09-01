4f691e8d34107f1d5a4f544093361928bdc604a5
fangmobile <fangjian@gmail.com>
2015-08-31 22:39:50 -0700

    add /cFy3159 to clear data to one 0, new data is added after 0, such 099 so average is not affected

--- app/controllers/Data.java
+++ app/controllers/Data.java
@@ -7,7 +7,9 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,6 +26,22 @@ public class Data extends Controller {
             Logger.error("file io error when writing data to file");
             e.printStackTrace();
         }
+        return ok();
+    }
+
+    public Result clear(){
+        Logger.info("clear data file:");
+        File datafile = Play.application().getFile("conf/data.txt");
+
+        try {
+            PrintWriter writer = new PrintWriter(datafile);
+            writer.print("0");
+            writer.close();
+        } catch (FileNotFoundException e) {
+            Logger.error("file io error when clearing data on file");
+            e.printStackTrace();
+        }
+
         return ok();
     }
 }
